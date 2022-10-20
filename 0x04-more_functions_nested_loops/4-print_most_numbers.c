43133fb67363b76d479544c0ba908983572bcf70
Aigbogun Osayuwamen <99479545+levis17@users.noreply.github.com>
2022-10-20 15:28:23 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-			_putchar(num % 10)
+			_putchar((num % 10) + '0')
 	}
 
-	_putchar('\n')
+	_putchar('\n');
 }
 
