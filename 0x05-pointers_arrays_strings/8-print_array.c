40abc7a17c55704055b1f6844309ab4920d293a7
Aigbogun Osayuwamen <99479545+levis17@users.noreply.github.com>
2022-10-24 23:19:04 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_array - Prints an inputted number of elements of an array of integers. 
+* print_array - Prints an inputted number of elements of an array of integers.
 * @a: The array of integers.
 * @n: The number of elements to be printed.
 */
