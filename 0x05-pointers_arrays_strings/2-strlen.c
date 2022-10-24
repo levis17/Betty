f43c173d54eb567129688594e56ee613d22765f0
Aigbogun Osayuwamen <99479545+levis17@users.noreply.github.com>
2022-10-24 22:07:22 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _strlen - Returns the length of a string.
-* @str: The string to get the length of.
+* @s: The string to get the length of.
 *
 * Return: The length of @str.
 */
