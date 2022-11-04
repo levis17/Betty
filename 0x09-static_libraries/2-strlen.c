b540d49da93dedd315443a73554834950ef05608
Osayuwamen <aigbogunosayu@gmail.com>
2022-11-04 16:00:07 -0700

    static libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - Returns the length of a string.
+* @s: The string to get the length of.
+*
+* Return: The length of @str.
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+
+	return (i);
+}
