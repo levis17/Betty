b791d54e00317f3eba73eb9c2cccfda96c55070f
Aigbogun Osayuwamen <99479545+levis17@users.noreply.github.com>
2022-11-10 15:41:05 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,7 +5,6 @@
 * ordered from min to max, inclusive.
 * @min: The first value of the array.
 * @max: The last value of the array.
-* @size: The size of the memory to print.
 *
 * Return: If min > max or the function fails - NULL.
 * Otherwise - a pointer to the newly created array.
