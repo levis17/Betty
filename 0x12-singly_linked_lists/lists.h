a1c1ee8a6fc94caacdb3c8398cf9d3f4312ebc49
Osayuwamen <aigbogunosayu@gmail.com>
2022-11-24 07:22:11 -0800

    list len

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,8 +5,17 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+* struct list_s - singly linked list
+* @str: string
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+
 typedef struct list_s
-{
+	{
 	char *str;
 	unsigned int len;
 	struct list_s *next;
